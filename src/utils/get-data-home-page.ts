ee77c6a8fe90d73be9d6914580b94b8d41ba0e94
Shafa Asyari Ramadhan <shafa.asyari.02@gmail.com>
2025-06-09 13:50:02 +0800

    feat: refactor query payload

--- src/utils/get-data-home-page.ts
+++ src/utils/get-data-home-page.ts
@@ -2,12 +2,28 @@ import { HomePageResponse } from "@/type";
 
 export const getDataHomePage: () => Promise<HomePageResponse> = async () => {
   try {
-    const res = await fetch(`${process.env.NEXT_URL_API}`, {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${process.env.NEXT_BEARER_TOKEN}`,
-      },
-    });
+    const query = new URLSearchParams({
+      "populate[CompanyInformation][populate]": "",
+      "populate[SEOSetting][populate]": "",
+      "populate[HeroSection][populate]": "",
+      "populate[AboutSection][populate]": "",
+      "populate[VisionAndMission][populate]": "",
+      "populate[ServiceSection][populate]": "",
+      "populate[IndustriesSection][populate]": "",
+      "populate[PartnerSection][populate][Partners][populate]": "",
+      "populate[InvestorSection][populate]": "",
+      "populate[ContactSection][populate]": "",
+    }).toString();
+
+    const res = await fetch(
+      `${process.env.NEXT_BASE_API}/cku-landing-page?${query}`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.NEXT_BEARER_TOKEN}`,
+        },
+      }
+    );
     const data = await res.json();
     return data;
   } catch (error) {
